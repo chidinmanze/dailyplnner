4eac065dcc8d744f247d2bf44f980b5f0f76d69c
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-07-16 11:48:17 -0400

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dailyplnner
+# Daily Planner
+
+The daily planner allows users to plan their work day, which will help organization and productivity. 
+
+## Features
+- Section at the top shows the current day and time
+- Time sections for 9 am - 5 pm allow user to input information
+- When the save button is clicked, the user input saves to the local storage
+
+## In Progress Features
+- Color coded input sections for past, current, and future times
+- Saved items reload in calendar when the page is refreshed
---
+++ assets/script.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    
+    // Set date and time info at the top of the calendar
+    var now = moment();
+    console.log(now.toString());
+    console.log(now.format("dddd MMMM Mo YYYY"));
+    $("#currentDay").html(now.format("dddd MMMM Mo YYYY h:m a"));
+
+    // 
+
+    // Save time and information typed in calendar line into local storage 
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+        let textIn = $(this).siblings(".input").val();
+        let time = $(this).parent().attr("id");
+        console.log(textIn)
+        console.log(time)
+        console.log(this);
+        console.log(event);
+        localStorage.setItem("textIn", textIn);
+        localStorage.setItem("time", time);
+
+        
+    });
+    
+
+    // Set past, present, and future color based on timeblock
+    // let time = $(this).parent().attr("id");
+    // function 
+    // var timeCLass = $(this).parent().attr("id");
+    // function timeClass()
+
+
+  
+
+
+});
+    
+
